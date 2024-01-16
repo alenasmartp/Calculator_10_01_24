f0715f95ab472edda1dc2051258bd4faad12ef5d
alenasmartp <150282056+alenasmartp@users.noreply.github.com>
2024-01-16 16:52:23 +0300

    Add files via upload

---
+++ Calculator_10_01_24/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Calculator");
+
+        int number1 = 77;
+        int number2 = 7;
+        int result1 = number1 + number2;
+        int result2 = number1 - number2;
+        int result3 = number1 * number2;
+        int result4 = number1 / number2;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
+
+    }
+}
